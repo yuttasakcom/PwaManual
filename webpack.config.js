2960b503e62de1a3c5ae54dbbdf594d507212984
YoProgrammer <yuttasakcom@gmail.com>
2017-07-02 09:54:24 +0700

    Edit tab to space from 4 to 2

--- webpack.config.js
+++ webpack.config.js
@@ -5,32 +5,32 @@ const workboxPlugin = require('workbox-webpack-plugin')
 const DIST_DIR = 'dist'
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, DIST_DIR),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-            inject: true,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeAttributeQuotes: true
-            },
-        }),
-        new workboxPlugin({
-            globPatterns: ['**/*.{html,js,css}'],
-            swDest: path.join(DIST_DIR, 'sw.js'),
-        })
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, DIST_DIR),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      },
+    }),
+    new workboxPlugin({
+      globPatterns: ['**/*.{html,js,css}'],
+      swDest: path.join(DIST_DIR, 'sw.js'),
+    })
+  ]
 }
