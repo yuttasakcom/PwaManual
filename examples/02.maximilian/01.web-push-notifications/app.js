dd5159155e1697d9a5c5eba3863ac7cf0200d968
YoProgrammer <yuttasakcom@gmail.com>
2017-12-18 17:06:07 +0700

    create Web Push Notification

--- examples/02.maximilian/01.web-push-notifications/app.js
+++ examples/02.maximilian/01.web-push-notifications/app.js
@@ -18,12 +18,80 @@ const enableNotificationsButtons = document.querySelectorAll(
 const displayConfirmNotification = () => {
   if ('serviceWorker' in navigator) {
     const options = {
-      body: 'You successfully subscribed to our Notification service!'
+      body: 'You successfully subscribed to our Notification service!',
+      icon: '/img/icons/app-icon-96x96.png',
+      image: '/img/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US',
+      vibrate: [100, 50, 200],
+      badge: '/img/icons/app-icon-96x96.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        {
+          action: 'confirm',
+          title: 'Okay',
+          icon: '/img/icons/app-icon-96x96.png'
+        },
+        {
+          action: 'cancel',
+          title: 'Cancel',
+          icon: '/img/icons/app-icon-96x96.png'
+        }
+      ]
     }
     navigator.serviceWorker.ready.then(swreg => {
-      swreg.showNotification('Successfully subscribed (from SW)!', options)
+      swreg.showNotification('Successfully subscribed!', options)
     })
   }
+}
+
+const configurePushSub = () => {
+  if (!('serviceWorker' in navigator)) {
+    return
+  }
+
+  let reg
+
+  navigator.serviceWorker.ready
+    .then(swreg => {
+      reg = swreg
+      return swreg.pushManager.getSubscription()
+    })
+    .then(sub => {
+      if (sub === null) {
+        let vapidPublicKey =
+          'BLFoheuwClsZevo-WB1smwPHZVdrToKlOTfOFUutxKvKxuqxhyWk23LcFrzdyhp9_nvoV12tmNut9Y54-DlUC-U'
+        let convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey)
+        return reg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: convertedVapidPublicKey
+        })
+      } else {
+      }
+    })
+    .then(newSub => {
+      console.log(newSub)
+      return fetch(
+        'https://elite-vault-181110.firebaseio.com/subscriptions.json',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+          },
+          body: JSON.stringify(newSub)
+        }
+      )
+    })
+    .then(res => {
+      if (res.ok) {
+        displayConfirmNotification()
+      }
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 const askForNotificationPermission = () => {
@@ -32,7 +100,8 @@ const askForNotificationPermission = () => {
     if (result !== 'granted') {
       console.log('No notification permission granted!')
     } else {
-      displayConfirmNotification()
+      configurePushSub()
+      //   displayConfirmNotification()
     }
   })
 }
@@ -45,4 +114,17 @@ if ('Notification' in window) {
       askForNotificationPermission
     )
   }
+}
+
+const urlBase64ToUint8Array = base64String => {
+  var padding = '='.repeat((4 - base64String.length % 4) % 4)
+  var base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+
+  var rawData = window.atob(base64)
+  var outputArray = new Uint8Array(rawData.length)
+
+  for (var i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
